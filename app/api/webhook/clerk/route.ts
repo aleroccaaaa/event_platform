6890f3316ca8fd2b9caeef34551fbc616e712105
Alessandro Rocca <roccaalessandro495@gmail.com>
2024-07-04 11:38:05 +0200

    first commit

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -63,7 +63,7 @@ export async function POST(req: Request) {
         username: username!,
         firstName: first_name!,
         lastName: last_name!,
-        photo: image_url
+        photo: image_url,
     }
 
     const newUser = await createUser(user);
--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -1,4 +1,4 @@
-import { Document, model, models, Schema } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface ICategory extends Document {
     _id: string;
